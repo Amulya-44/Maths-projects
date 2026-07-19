85114b2c2c57055b7b12adbc5d711f6bb5d1c2de
Amulya <amullyashrestha4848.in>
2026-07-19 13:59:40 +0545

    Add new folder to Maths project

---
+++ Prime Number Sieve of Eratosthenes/Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void displayPrime(int num) 
+{
+ 
+    vector<bool> is_prime(num + 1, true);
+
+    if (num >= 0)
+    {
+         is_prime[0] = false;
+         is_prime[1] = false;
+    }
+    
+    int limit = sqrt(num);
+    for (int p = 2; p <= limit; p++)
+    {
+        if (is_prime[p])
+        {
+            for (int i = p * p; i <= num; i += p) 
+            {
+                is_prime[i] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i <= num; i++) 
+    {
+        if (is_prime[i])
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}a
+
+int main()
+{
+    int num;
+    cout << "Enter a number up to which primes are to be determined: ";
+    cin >> num;
+    displayPrime(num);
+    return 0;
+}
