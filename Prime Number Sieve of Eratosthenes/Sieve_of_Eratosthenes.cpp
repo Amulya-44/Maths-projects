efe2772dd83814458e5dad3135e112ce386a1732
Amulya-44 <amullyashrestha4848@gmail.com>
2026-07-19 14:12:28 +0545

    updated edge case

--- Prime Number Sieve of Eratosthenes/Sieve_of_Eratosthenes.cpp
+++ Prime Number Sieve of Eratosthenes/Sieve_of_Eratosthenes.cpp
@@ -4,44 +4,53 @@
 
 using namespace std;
 
-void displayPrime(int num) 
+void displayPrime(int num)
 {
- 
+    if (num < 2)
+    {
+        cout << "No prime numbers." << endl;
+        return;
+    }
+
     vector<bool> is_prime(num + 1, true);
 
-    if (num >= 0)
-    {
-         is_prime[0] = false;
-         is_prime[1] = false;
-    }
-    
+    is_prime[0] = false;
+    is_prime[1] = false;
+
     int limit = sqrt(num);
+
     for (int p = 2; p <= limit; p++)
     {
         if (is_prime[p])
         {
-            for (int i = p * p; i <= num; i += p) 
+            for (int i = p * p; i <= num; i += p)
             {
                 is_prime[i] = false;
             }
         }
     }
 
-    for (int i = 2; i <= num; i++) 
+    cout << "Prime numbers are: ";
+
+    for (int i = 2; i <= num; i++)
     {
         if (is_prime[i])
         {
             cout << i << " ";
         }
     }
+
     cout << endl;
-}a
+}
 
 int main()
 {
     int num;
+
     cout << "Enter a number up to which primes are to be determined: ";
     cin >> num;
+
     displayPrime(num);
+
     return 0;
 }
